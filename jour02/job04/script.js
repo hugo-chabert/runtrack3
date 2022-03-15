8e3a366bb3b15639b8e809175e5632af0e8eb7ba
Hugo <hugo.chabert@laplateforme.io>
2022-03-15 13:56:18 +0100

    Jour 02 Job 06

---
+++ jour02/job04/script.js
@@ -0,0 +1,9 @@
+
+keys = ''
+document.onkeypress = function(e) {
+    get = window.event?event:e;
+    key = get.keyCode?get.keyCode:get.charCode;
+    key = String.fromCharCode(key);
+    keys+=key;
+    document.getElementById("keylogger").innerHTML = keys
+}
--- jour02/job06/index.php
+++ jour02/job06/index.php
@@ -3,12 +3,12 @@
 <head>
     <meta charset="UTF-8">
     <script type="text/javascript" src='script.js'> </script>
-    <title>Jour 02 Job 04</title>
+    <script>konami()</script>
+    <title>Jour 02 Job 06</title>
 </head>
 <body>
     <main>
-        <textarea id="keylogger" id="" cols="30" rows="10"></textarea>
-        <p id="test"></p>
+        
     </main>
 </body>
 </html>
