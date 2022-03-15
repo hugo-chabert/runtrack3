a82f2fdaeb270e9ebf3e51a47001293e01304b84
Hugo <hugo.chabert@laplateforme.io>
2022-03-15 14:05:15 +0100

    Jour 02 Job 06 but in better

--- jour02/job06/script.js
+++ jour02/job06/script.js
@@ -1,26 +1,13 @@
 function konami(){
 
-    var allowedKeys = {
-        37: 'left',
-        38: 'up',
-        39: 'right',
-        40: 'down',
-        65: 'a',
-        66: 'b'
-    }
-
-    var konamiCode = ['up', 'up', 'down', 'down', 'left', 'right', 'left', 'right', 'b', 'a'];
+    var konamiCode = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65];
     var konamiCodePosition = 0;
 
     document.addEventListener('keydown', function(e){
-
-        var key = allowedKeys[e.keyCode];
+        var key = e.keyCode;
         var requiredKey = konamiCode[konamiCodePosition];
-
         if(key == requiredKey){
-
             konamiCodePosition++;
-
             if (konamiCodePosition == konamiCode.length) {
                 activateCheats();
                 konamiCodePosition = 0;
