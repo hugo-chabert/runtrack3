861272b1924103623afacf7c97abcc49b5e49fbd
Hugo <hugo.chabert@laplateforme.io>
2022-03-16 11:50:44 +0100

    Jour 01 Job02 Without win/loose

--- jour03/job02/index.php
+++ jour03/job02/index.php
@@ -10,16 +10,16 @@
 <body>
     <main>
         <button id="button">Mélanger</button>
-        <div class="rangees">
+        <div id="rangees">
 
         </div>
         <div id="melangees">
-            <div class="img1"><img src="img/arc1.png" alt=""></div>
-            <div class="img2"><img src="img/arc2.png" alt=""></div>
-            <div class="img3"><img src="img/arc3.png" alt=""></div>
-            <div class="img4"><img src="img/arc4.png" alt=""></div>
-            <div class="img5"><img src="img/arc5.png" alt=""></div>
-            <div class="img6"><img src="img/arc6.png" alt=""></div>
+            <div id="img1"><img src="img/arc1.png" alt=""></div>
+            <div id="img2"><img src="img/arc2.png" alt=""></div>
+            <div id="img3"><img src="img/arc3.png" alt=""></div>
+            <div id="img4"><img src="img/arc4.png" alt=""></div>
+            <div id="img5"><img src="img/arc5.png" alt=""></div>
+            <div id="img6"><img src="img/arc6.png" alt=""></div>
         </div>
     </main>
 </body>
