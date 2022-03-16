8147c52eedfa60493c1576cdf8e1df39818f3d2d
Hugo <hugo.chabert@laplateforme.io>
2022-03-16 11:18:17 +0100

    Jour 01 Job02 not finished

---
+++ jour03/job02/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="style.css">
+    <script src="https://code.jquery.com/jquery-3.6.0.js" integrity="sha256-H+K7U5CnXl1h5ywQfKtSj8PCmoN9aaq30gDh27Xc0jk=" crossorigin="anonymous"></script>
+    <script type='text/javascript' src='script.js'></script>
+    <title>Jour 03 Job 02</title>
+</head>
+<body>
+    <main>
+        <button id="button">Mélanger</button>
+        <div class="img1"><img src="img/arc1.png" alt=""></div>
+        <div class="img2"><img src="img/arc2.png" alt=""></div>
+        <div class="img3"><img src="img/arc3.png" alt=""></div>
+        <div class="img4"><img src="img/arc4.png" alt=""></div>
+        <div class="img5"><img src="img/arc5.png" alt=""></div>
+        <div class="img6"><img src="img/arc6.png" alt=""></div>
+    </main>
+</body>
+</html>
---
+++ jour03/job02/script.js
@@ -0,0 +1,11 @@
+$(document).ready(function (){
+    let btn = $("#button")
+    let div1 = $("img1")
+    let div2 = $("img2")
+    let div3 = $("img3")
+    let div4 = $("img4")
+    let div5 = $("img5")
+    let div6 = $("img6")
+
+    
+})
