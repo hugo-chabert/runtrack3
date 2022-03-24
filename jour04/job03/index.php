c320a455c4a2d213adb3315f42e61ecbd4ed365b
Hugo <hugo.chabert@laplateforme.io>
2022-03-24 11:39:57 +0100

    Jour04 job03

--- jour04/job03/index.php
+++ jour04/job03/index.php
@@ -1,36 +1,19 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
     <meta charset="UTF-8">
-    <script src="https://code.jquery.com/jquery-3.6.0.js" integrity="sha256-H+K7U5CnXl1h5ywQfKtSj8PCmoN9aaq30gDh27Xc0jk=" crossorigin="anonymous"></script>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <script type='text/javascript' src='script.js'></script>
-    <title>Jour 04 Job 03</title>
+    <title>Document</title>
 </head>
 <body>
-    <main>
-        <form action="">
-            <input type="text" name="id" placeholder="ID">
-            <input type="text" name="name" placeholder="Name">
-            <select name="type">
-                <option value="">--Please choose an option--</option>
-                <option value="fighting">Fighting</option>
-                <option value="dragon">Dragon</option>
-                <option value="water">Water</option>
-                <option value="electric">Electric</option>
-                <option value="fire">Fire</option>
-                <option value="ice">Ice</option>
-                <option value="bug">Bug</option>
-                <option value="normal">Normal</option>
-                <option value="grass">Grass</option>
-                <option value="poison">Poison</option>
-                <option value="psychic">Psychic</option>
-                <option value="rock">Rock</option>
-                <option value="ground">Ground</option>
-                <option value="ghost">Ghost</option>
-                <option value="flying">Flying</option>
-            </select>
-            <button name="button">Click here</button>
-        </form>
-    </main>
+
+    <select name="type" id="type">
+    </select>
+
+    <button id='filter'> Filtrer </button>
+    <button id='reset'> Reset </button>
+
 </body>
 </html>
